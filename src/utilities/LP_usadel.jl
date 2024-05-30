0c52df032a1e24e804b31a4cee208b29b19a09b8
Carlos Payá <carlos.paya@csic.es>
2024-05-30 12:31:12 +0200

    usadel updated

--- src/utilities/LP_usadel.jl
+++ src/utilities/LP_usadel.jl
@@ -4,16 +4,19 @@ function pairbreaking(Φ, n, Δ0, ξd, R, d)
     return Λ
 end
 
+function ps_sign(x)
+    return real(x) < 0 ? -1 : 1
+end
+
 function uUsadel(Δ0, Λ, ω)
     Δd = Δ0 * (1 - π/4 * Λ/Δ0 - π^2/32 * (Λ/Δ0)^2 - π^3/96 * (Λ/Δ0)^3)
     pep = complex(-Δd^6 + 3 * Δd^4 * (Λ^2 + ω^2) + (Λ^2 + ω^2)^3 - 3 * Δd^2 * (Λ^4 - 16 * Λ^2 * ω^2 + ω^4) + 
     6 * Δd * Λ * sqrt(complex(-3*(Δd^2 - Λ^2)^3 * ω^2 + 9 * (Δd^4 + 7 * Δd^2 * Λ^2 + Λ^4) * ω^4 + 9 * (-Δd^2 + Λ^2) * ω^6 + 3 * ω^8)))^(1/3)
     nun = ω^2 - Δd^2 + Λ^2
     rai = sqrt(complex(ω^2 - 2 * nun / 3 + nun^2 / (3 * pep) + pep / 3))
-    usa = 1/(2 * Δd) *(ω + sign(ω) * rai - sign(ω) * sqrt(complex(2 * ω^2 - 4 * nun/3 - nun^2 /(3 * pep) - pep/ 3 - sign(ω) * 2 * (Δd^2 + Λ^2) * ω / rai)))
+    usa = 1/(2 * Δd) *(ω + ps_sign(ω) * rai - ps_sign(ω) * sqrt(complex(2 * ω^2 - 4 * nun/3 - nun^2 /(3 * pep) - pep/ 3 - ps_sign(ω) * 2 * (Δd^2 + Λ^2) * ω / rai)))
     return usa
 end
-
 function LP_lobe(n, ξd, R, d)
     RLP = R + d/2
     pre = 1.76 * π * exp(-π/4) / 4
