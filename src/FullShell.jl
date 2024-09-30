a44897783f57d395862f86879f54af8751d2e874
Carlos Payá <carlos.paya@csic.es>
2024-09-30 16:01:58 +0200

    uUsadel updated to be analyitical in the full complex plane

--- src/FullShell.jl
+++ src/FullShell.jl
@@ -8,7 +8,7 @@ export build_cyl, build_cyl_mm, ΣS3DUsadel, pairbreaking, LP_lobe, ΔΛ
 
 export Params, Params_mm
 
-export σ0τx, σ0τy, σ0τz, σ0τ0, σzτ0, σzτz, σyτy, σyτz, σyτ0, σxτz, σxτ0, σ0, σx, σy, σz, c_up, c_down
+export σ0τx, σ0τy, σ0τz, σ0τ0, σzτ0, σzτz, σyτy, σyτz, σyτ0, σxτz, σxτ0, σ0, σx, σy, σz, c_up, c_down, Ω
 
 include("utilities/LP_usadel.jl")
 include("utilities/pauli_products.jl") 
--- src/utilities/LP_usadel.jl
+++ src/utilities/LP_usadel.jl
@@ -6,10 +6,10 @@ function pairbreaking(Φ, n, Δ0, ξd, R, d)
     return Λ
 end
 
-function ps_sign(x)
-    #return real(x) < 0 ? -1.0 + imag(x)*im*1.0 : 1.0 + imag(x)*im*1.0
-    return -1.0 + 1e-5im
-end
+# function ps_sign(x)
+#     return real(x) < 0 ? -1.0 + imag(x)*im*1.0 : 1.0 + imag(x)*im*1.0
+#     #return -1.0 + 1e-5im
+# end
 
 function ΔD(Λ, Δ0, ω)
     Δd = ΔΛ(real(Λ), real(Δ0))
@@ -24,19 +24,33 @@ function Ω(Λ, Δ0, ω)
     return (ΔD(Λ, Δ0, ω)^(2/3) -  Λ^(2/3))^(3/2)
 end
 
-function usimple(Δ0, Λ, ω)
-    return (ω/Ω(Λ, Δ0, ω)) * sqrt(complex(1 - (ω/Ω)^2))
-end
+# function usimple(Δ0, Λ, ω)
+#     return (ω/Ω(Λ, Δ0, ω)) * sqrt(complex(1 - (ω/Ω)^2))
+# end
 
+# function uUsadel(Δ0, Λ, ω)
+#     ω = ifelse(real(ω) == 0, imag(ω) + imag(ω)*im, ω)
+#     #Δd = Δ0 * (1 - π/4 * Λ/Δ0 - π^2/32 * (Λ/Δ0)^2 - π^3/96 * (Λ/Δ0)^3)
+#     Δd = ΔD(Λ, Δ0, ω)
+#     pep = complex(-Δd^6 + 3 * Δd^4 * (Λ^2 + ω^2) + (Λ^2 + ω^2)^3 - 3 * Δd^2 * (Λ^4 - 16 * Λ^2 * ω^2 + ω^4) + 
+#     6 * Δd * Λ * sqrt(complex(-3*(Δd^2 - Λ^2)^3 * ω^2 + 9 * (Δd^4 + 7 * Δd^2 * Λ^2 + Λ^4) * ω^4 + 9 * (-Δd^2 + Λ^2) * ω^6 + 3 * ω^8)))^(1/3)
+#     nun = ω^2 - Δd^2 + Λ^2
+#     rai = sqrt(complex(ω^2 - 2 * nun / 3 + nun^2 / (3 * pep) + pep / 3))
+#     usaIn = 1/(2 * Δd) *(ω + ps_sign(ω) * rai - ps_sign(ω) * sqrt(complex(2 * ω^2 - 4 * nun/3 - nun^2 /(3 * pep) - pep/ 3 - ps_sign(ω) * 2 * (Δd^2 + Λ^2) * ω / rai)))
+#     return  usaIn
+# end
+
+# Removed ps_sign so uUsadel is analitical in the full complex plane. 
+# WARNING: it is defined only for NEGATIVE frequencies.
 function uUsadel(Δ0, Λ, ω)
-    ω = ifelse(real(ω) == 0, imag(ω) + imag(ω)*im, ω)
+    #ω = ifelse(real(ω) == 0, imag(ω) + imag(ω)*im, ω)
     #Δd = Δ0 * (1 - π/4 * Λ/Δ0 - π^2/32 * (Λ/Δ0)^2 - π^3/96 * (Λ/Δ0)^3)
     Δd = ΔD(Λ, Δ0, ω)
     pep = complex(-Δd^6 + 3 * Δd^4 * (Λ^2 + ω^2) + (Λ^2 + ω^2)^3 - 3 * Δd^2 * (Λ^4 - 16 * Λ^2 * ω^2 + ω^4) + 
     6 * Δd * Λ * sqrt(complex(-3*(Δd^2 - Λ^2)^3 * ω^2 + 9 * (Δd^4 + 7 * Δd^2 * Λ^2 + Λ^4) * ω^4 + 9 * (-Δd^2 + Λ^2) * ω^6 + 3 * ω^8)))^(1/3)
     nun = ω^2 - Δd^2 + Λ^2
     rai = sqrt(complex(ω^2 - 2 * nun / 3 + nun^2 / (3 * pep) + pep / 3))
-    usaIn = 1/(2 * Δd) *(ω + ps_sign(ω) * rai - ps_sign(ω) * sqrt(complex(2 * ω^2 - 4 * nun/3 - nun^2 /(3 * pep) - pep/ 3 - ps_sign(ω) * 2 * (Δd^2 + Λ^2) * ω / rai)))
+    usaIn = 1/(2 * Δd) *(ω - rai + sqrt(complex(2 * ω^2 - 4 * nun/3 - nun^2 /(3 * pep) - pep/ 3 + 2 * (Δd^2 + Λ^2) * ω / rai)))
     return  usaIn
 end
 
