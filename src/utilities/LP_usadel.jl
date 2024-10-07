e65bf9b1f9363af340476ced0cbf246e77d41c29
Carlos Payá <carlos.paya@csic.es>
2024-10-07 16:49:42 +0200

    add iomegamax

--- src/utilities/LP_usadel.jl
+++ src/utilities/LP_usadel.jl
@@ -61,7 +61,7 @@ function itip(Δ0, Λ)
     d2 = (2 + 345 * x^2 - 345 * x^4 - 2 * x^6 + 9 * sqrt(3) * x * (1 + x^2) * sqrt(8 + 359 * x^2 + 8 * x^4))^(1/3)
     p2 = n2 / d2
     p3 = 2^(2/3) * d2
-    return sqrt((p1 + p2 + p3) / 6) * Δd
+    return sqrt(complex((p1 + p2 + p3) / 6)) * Δd
 end
 
 function LP_lobe(n, ξd, R, d)
