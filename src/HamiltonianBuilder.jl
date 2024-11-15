25520cd55e1cb1daf88d345cd19bf90be5e57a00
Carlos Payá <carlos.paya@csic.es>
2024-11-15 15:56:37 +0100

    add ballistic

--- src/HamiltonianBuilder.jl
+++ src/HamiltonianBuilder.jl
@@ -41,7 +41,6 @@ end
 
 # Hamiltonian constructor 
 ΣS3DUsadel(Δ0, Λ, ω;) = - Δ0 *(uUsadel(Δ0, Λ, ω) * σ0τ0 - σ0τx) / sqrt(complex(1-uUsadel(Δ0, Λ, ω)^2))
-Ω(Λ, Δ0) = (ΔD(Λ, Δ0)^(2/3) - Λ^(2/3))^(3/2) 
 ΣS3DBallistic(Δ0, Λ, ω;) = - Δ0 *( (ω/Ω(Λ, Δ0)) * σ0τ0 - σ0τx) / sqrt(complex(1-(ω/Ω(Λ, Δ0))^2))
 #ΣS3DUsadel(Δ0, Λ, ω;) = - Δ0 *(usimple(Δ0, Λ, ω) * σ0τ0 - σ0τx) / sqrt(complex(1-usimple(Δ0, Λ, ω)^2))
 ΣΔ(Δ0, Λ, ω;) = (ΔD(Λ, Δ0)^(2/3) - Λ^(2/3))^(3/2) * σ0τx
