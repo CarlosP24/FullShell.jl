3fa036da05011471f892cd78683c1e53b4487642
Carlos Payá <carlos.paya@csic.es>
2025-04-03 12:01:33 +0200

    add previous version of symmetric Usadel funciton

--- src/HamiltonianBuilder.jl
+++ src/HamiltonianBuilder.jl
@@ -40,6 +40,7 @@ end
 
 # Hamiltonian constructor 
 ΣS3DUsadel(Δ0, Λ, ω;) = - Δ0 *(uUsadel(Δ0, Λ, ω) * σ0τ0 - σ0τx) / sqrt(complex(1-uUsadel(Δ0, Λ, ω)^2))
+ΣS3DUsadel_old(Δ0, Λ, ω;) = - Δ0 *(uUsadel_old(Δ0, Λ, ω) * σ0τ0 - σ0τx) / sqrt(complex(1-uUsadel_old(Δ0, Λ, ω)^2))
 ΣS3DBallistic(Δ0, Λ, ω;) = - Δ0 *((ω/Ω(Λ, Δ0)) * σ0τ0 - σ0τx) / sqrt(complex(1-(ω/Ω(Λ, Δ0))^2))
 #ΣS3DUsadel(Δ0, Λ, ω;) = - Δ0 *(usimple(Δ0, Λ, ω) * σ0τ0 - σ0τx) / sqrt(complex(1-usimple(Δ0, Λ, ω)^2))
 ΣΔ(Δ0, Λ, ω;) = (ΔD(Λ, Δ0)^(2/3) - Λ^(2/3))^(3/2) * σ0τx
@@ -100,6 +101,8 @@ function build_cyl(p::Params; nforced = nothing, phaseshifted = false)
       ΣS = ΣS3DUsadel
     elseif shell == "Ballistic"
       ΣS = ΣS3DBallistic
+    elseif shell == "Usadel_old"
+      ΣS = ΣS3DUsadel_old
     else
       ΣS = ΣΔ
     end
