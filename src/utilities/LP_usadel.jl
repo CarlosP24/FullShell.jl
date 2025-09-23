ca1b717b1f266d513bca4e2594d2319372450e40
Carlos Payá <carlos.paya@csic.es>
2025-09-23 16:58:12 +0200

    debug

--- src/utilities/LP_usadel.jl
+++ src/utilities/LP_usadel.jl
@@ -20,12 +20,7 @@ function ΔD(Λ, Δ0)
 end
 
 function Ω(Λ, Δ0)
-    try
-        gap = (ΔD(Λ, Δ0)^(2/3) -  Λ^(2/3))^(3/2)
-    catch e 
-        @warn "Gap is complex. Setting gap to 0. Original error: $e"
-        gap = 0
-    end
+    return real(complex((complex(ΔD(Λ, Δ0))^(2/3) -  complex(Λ)^(2/3)))^(3/2))
 end
 
 # function usimple(Δ0, Λ, ω)
